8ce26a3348d91cf2e6f7206d77d2c9e8c5b54942
Ray <rayseptian741@gmail.com>
2025-04-17 15:26:57 +0800

    Initial Commit

---
+++ 卓建佑_果思作業/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  卓建佑_果思作業
+//
+//  Created by Ray Septian Togi on 2025/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
