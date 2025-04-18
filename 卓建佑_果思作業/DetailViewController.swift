c0e62036530c9a4bce047f1e239ceebfca41a711
Ray <rayseptian741@gmail.com>
2025-04-18 15:56:15 +0800

    Finished Homework

---
+++ 卓建佑_果思作業/DetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewController.swift
+//  卓建佑_果思作業
+//
+//  Created by Ray Septian Togi on 2025/4/18.
+//
+
+import Foundation
---
+++ 卓建佑_果思作業/RepositoryCell.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryCell.swift
+//  卓建佑_果思作業
+//
+//  Created by Ray Septian Togi on 2025/4/18.
+//
+
+import Foundation
